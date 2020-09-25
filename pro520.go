512d8400ac2448f41d3f733a40dba8987fa5e11d
Daniel Randall <dgrandall@me.com>
2020-09-25 10:32:21 -0600

    StreamJPEG() function

--- pro520.go
+++ pro520.go
@@ -88,6 +88,37 @@ func (c *Pro520) Stream(ctx context.Context) (chan image.Image, chan error, erro
 	return images, errs, nil
 }
 
+func (c *Pro520) StreamJPEG(ctx context.Context) (chan []byte, chan error, error) {
+	tok, err := c.getToken(ctx)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to login to camera: %w", err)
+	}
+
+	images := make(chan []byte)
+	errs := make(chan error)
+	go func() {
+		ticker := time.NewTicker(125 * time.Millisecond)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				image, err := c.getLiveJPEG(ctx, tok)
+				if err != nil {
+					errs <- err
+					continue
+				}
+
+				images <- image
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return images, errs, nil
+}
+
 func (c *Pro520) Snapshot(ctx context.Context) (image.Image, error) {
 	tok, err := c.getToken(ctx)
 	if err != nil {
@@ -147,13 +178,8 @@ func (c *Pro520) SetPreset(ctx context.Context, preset int) error {
 	}
 	defer resp.Body.Close()
 
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("unable to read response: %w", err)
-	}
-
 	if resp.StatusCode/100 != 2 {
-		return fmt.Errorf("unable to set preset, bad statuscode received: %s", respBody)
+		return fmt.Errorf("statuscode %d", resp.StatusCode)
 	}
 
 	return nil
@@ -180,19 +206,28 @@ func (c *Pro520) Reboot(ctx context.Context) error {
 	}
 	defer resp.Body.Close()
 
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("unable to read response: %w", err)
-	}
-
 	if resp.StatusCode/100 != 2 {
-		return fmt.Errorf("unable to reboot camera, bad statuscode : %s", respBody)
+		return fmt.Errorf("statuscode %d", resp.StatusCode)
 	}
 
 	return nil
 }
 
 func (c *Pro520) getLiveImage(ctx context.Context, token string) (image.Image, error) {
+	jpeg, err := c.getLiveJPEG(ctx, token)
+	if err != nil {
+		return nil, err
+	}
+
+	image, _, err := image.Decode(bytes.NewReader(jpeg))
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode image: %w", err)
+	}
+
+	return image, nil
+}
+
+func (c *Pro520) getLiveJPEG(ctx context.Context, token string) ([]byte, error) {
 	ctx, cancel := context.WithTimeout(ctx, 500*time.Millisecond)
 	defer cancel()
 
@@ -211,12 +246,12 @@ func (c *Pro520) getLiveImage(ctx context.Context, token string) (image.Image, e
 	}
 	defer resp.Body.Close()
 
-	image, _, err := image.Decode(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("unable to decode image: %w", err)
+		return nil, fmt.Errorf("unable to read response: %w", err)
 	}
 
-	return image, nil
+	return body, nil
 }
 
 type pro520Login struct {
